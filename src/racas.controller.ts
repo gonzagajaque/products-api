130351412da62360cb5545b17cbbe7815c505214
Jaqueline Gonzaga <jaqueline_gonzaga@hotmail.com>
2022-05-08 21:31:34 -0400

    feat: models

--- src/racas.controller.ts
+++ src/racas.controller.ts
@@ -7,31 +7,40 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-
+import { Raca } from './raca.model';
 @Controller('racas')
 export class RacasController {
+  racas: Raca[] = [
+    new Raca('LIV01', 'livro TDD e BDD na prática', 29.9),
+    new Raca('LIV02', 'Iniciando com Flutter', 39.9),
+    new Raca('LIV03', 'Livro Inteligência Artificial como serviço', 29.9),
+  ];
+
   @Get()
-  obterTodos(): string {
-    return 'Lista de racas';
+  obterTodos(): Raca[] {
+    return this.racas;
   }
 
   @Get(':id')
-  obterUm(@Param() params): string {
-    return `Retorna os dados das racas ${params.id}`;
+  obterUm(@Param() params): Raca {
+    return this.racas[0];
   }
 
   @Post()
-  criar(@Body() raca): string {
-    return 'Raca criada';
+  criar(@Body() raca: Raca) {
+    raca.id = 100;
+    this.racas.push(raca);
   }
 
   @Put()
-  alterar(@Body() raca): string {
-    return 'Raca atualizada';
+  alterar(@Body() raca: Raca) {
+    console.log(raca);
+
+    return raca;
   }
 
   @Delete(':id')
-  apagar(@Param() params): string {
-    return `Apaga as racas ${params.id}`;
+  apagar(@Param() params) {
+    this.racas.pop();
   }
 }
